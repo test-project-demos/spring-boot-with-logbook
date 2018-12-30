d46567cd918322cbd653ac4eb9be6633ab5a0a30
Cnfn <cnfnss@gmail.com>
2018-12-30 22:51:46 +0800

    first commit

---
+++ src/main/java/com/example/springbootwithlogbook/SpringBootWithLogbookApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootwithlogbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithLogbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithLogbookApplication.class, args);
+	}
+
+}
+
